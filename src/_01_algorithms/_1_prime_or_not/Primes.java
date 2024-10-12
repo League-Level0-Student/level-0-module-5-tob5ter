d2808eae04ad0a813ac9650bf4ab572c47f10d99
league <league@wts14-3.attlocal.net>
2024-10-11 18:30:49 -0700

    Prime Day!

---
+++ src/_01_algorithms/_1_prime_or_not/Primes.java
@@ -0,0 +1,20 @@
+package _01_algorithms._1_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+public class Primes {
+	public static void main(String[] args) {
+		String number = JOptionPane.showInputDialog("type a random number");
+		int num = Integer.parseInt(number);
+		for(int i = 2; i<num;i++) {
+			System.out.println(num%i);
+			if(num%i == 0) {
+				System.out.println("your number is not prime");
+				return;
+			}
+			
+		}
+		System.out.println("Your number is prime");
+		
+}
+}
